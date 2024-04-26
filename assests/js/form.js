2b54e8b255750f2d47230a9502e32ee7231215f5
Sephora Jean <sephorejean@gmail.com>
2024-04-26 00:10:49 -0400

    second commit

--- assests/js/form.js
+++ assests/js/form.js
@@ -2,11 +2,16 @@ function formFunc(event){
     event.preventDefault();
     let username = document.getElementById('username').value;
     let blogTitle = document.getElementById('title').value;
-    let blogContent = document.getElementById('blogContent').value;
+    let blogContent = document.getElementById('blog-content').value;
 
     localStorage.setItem('ls_username', username);
     localStorage.setItem('ls_blogtitle', blogTitle);
     localStorage.setItem('ls_blogContent', blogContent);
 
-    
+    console.log(localStorage.getItem('ls_username'));
+    console.log(localStorage.getItem('ls_blogtitle'));
+
+    return true;
+
+
 }
